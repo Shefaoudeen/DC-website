ae4bd661b7dbee86854ae01a3a1eea4943ec0ad9
Shefa <shefaoudeen123@gmail.com>
2024-03-21 21:32:28 +0530

    added division

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 import Arrow from "./Arrow";
-import { rotateIn } from "react-animations";
+import { work } from "../Data/work";
 
 const Work = () => {
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(null);
 
-  const handleClick = () => {
-    setToggle(toggle === true ? false : true);
+  const handleClick = (getId) => {
+    setToggle(toggle === null ? getId : null);
   };
 
-  const rotateDiv = styled.div`
-    animation: 1s ${bounceAnimation};
-  `;
+  let red = "ff1f25";
+  let blue = "005cef";
 
   return (
     <div className="flex justify-center items-center w-screen ">
@@ -21,48 +20,68 @@ const Work = () => {
             Our Work.
           </h1>
         </div>
-        <div
-          onClick={() => handleClick()}
-          className={`flex flex-col justify-center w-[80%] h-[${
-            toggle === true ? 1000 : 200
-          }px] border-b-[10px] border-[#005cef] cursor-pointer`}
-        >
-          <div className="flex flex-col justify-center relative h-[200px]">
-            <h1 className="text-[#ff1f25] text-2xl font-semibold">
-              Brand Idents
-            </h1>
-            <h1 className="text-[#ff1f25] text-7xl font-bold">MakeReign.</h1>
-            <div
-              className={`absoulte w-[50px] h-[100px] absolute right-2 rotate-${
-                toggle === true ? 180 : null
-              }`}
-            >
-              <Arrow className={`animate-${toggle === true ? "spin" : null}`} />
-            </div>
-          </div>
-          {toggle === true ? (
-            <div className="h-[600px] flex bg-[#ff1f25]">
-              <div className="w-1/2 h-full bg-[#005cef]"></div>
-              <div className="flex  gap-10 justify-center flex-col items-center w-1/2 h-full bg-rose-600">
-                <div className="text-white text-5xl font-bold font-sans w-[80%]">
-                  <h1>MakeReign's new</h1>
-                  <h1>look comes to life.</h1>
-                </div>
-                <div className="w-[80%] text-xl text-white">
-                  <h1>
-                    A set of animated idents built to introduce MakeReign's 2023
-                    reel. Killer branding, meet killer visuals.
+        {work.map((dataItems) => {
+          return (
+            <div className="w-[100%] flex flex-col items-center">
+              <div
+                onClick={() => handleClick(dataItems.id)}
+                className={`flex flex-col justify-center w-[80%] border-b-[${
+                  toggle == dataItems.id ? 0 : 10
+                }px] border-[#005cef] cursor-pointer`}
+              >
+                <div className="flex flex-col justify-center relative h-[200px]">
+                  <h1
+                    className={`text-[#${
+                      toggle == dataItems.id ? blue : red
+                    }] text-2xl font-semibold transition-colors delay-25 ease-linear`}
+                  >
+                    {dataItems.motto}
                   </h1>
-                </div>
-                <div className="flex justify-start w-[80%]">
-                  <div className="bg-yellow-400 justify-start px-6 py-4 font-bold text-xl">
-                    View Project
+                  <h1
+                    className={`text-[#${
+                      toggle == dataItems.id ? blue : red
+                    }] text-7xl font-bold transition-colors delay-25 ease-linear`}
+                  >
+                    {dataItems.division}
+                  </h1>
+                  <div
+                    className={`absoulte w-[50px] h-[100px] absolute right-2 rotate-${
+                      toggle === true ? 180 : null
+                    }`}
+                  >
+                    <Arrow
+                      className={`animate-${toggle === true ? "spin" : null}`}
+                    />
                   </div>
+                </div>
+                <div className="transition-trasform delay-500 ease-linear">
+                  {toggle === dataItems.id ? (
+                    <div className="h-[600px] flex bg-[#ff1f25] transition-all delay-300 ease-linear">
+                      <div className="w-1/2 h-full bg-[#005cef]"></div>
+                      <div className="flex  gap-10 justify-center flex-col items-center w-1/2 h-full bg-rose-600">
+                        <div className="flex  gap-10 justify-center flex-col items-center w-full h-full bg-rose-600">
+                          <div className="text-white text-5xl font-bold font-sans w-[80%]">
+                            <h1>{dataItems.division}</h1>
+                          </div>
+                          <div className="w-[78%] text-xl text-white block">
+                            <span>{dataItems.description1}</span>
+                            <span className="font-bold">{dataItems.works}</span>
+                            <span>{dataItems.description2}</span>
+                          </div>
+                          <div className="flex justify-start w-[80%]">
+                            <div className="bg-yellow-400 justify-start px-6 py-4 font-bold text-xl">
+                              View Project
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ) : null}
                 </div>
               </div>
             </div>
-          ) : null}
-        </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/Data/work.js
@@ -0,0 +1,32 @@
+const work = [
+  {
+    id: 1,
+    division: "DC PRIME",
+    description1:
+      "Welcome  to PRIME! We specialize in crafting compelling visuals using adobe photoshop and illustrator. With projects like ",
+    works: "REJOUIR, NEURA, MAGAZINES and UNIVERSITY CALENDERS",
+    description2:
+      ", our goals is to leave a everlasting impression with powerful designs",
+    motto: "Innovate, Create, Design!",
+  },
+  {
+    id: 2,
+    division: "DC VIDEO",
+    description1:
+      "Welcome  to VIDEO! where we harness After Effects, Premiere Pro, Blender, Da Vinci Resolve, Izotope and More! Past projects feature ",
+    works: "UNIVERSITY INTRO, SYMPOSIUM TRAILERS and BEYOND",
+    description2: ". Let's craft cinematic experiences together",
+    motto: "Lights, Camera, Edit!",
+  },
+  {
+    id: 3,
+    division: "DC DEVELOPMENT",
+    description1: "Welcome  to PRIME! We create and maintain ",
+    works: "UNIVERSITY WEBSITE and PORTFOLIOS ",
+    description2:
+      ", with technologies like REACT, NODE AND VARIOUS JAVASCRIPT FRAMEWORKS. From concept to execution, Let's collaborate to realize your digital vision!",
+    motto: "Coding Dreams into Reality!",
+  },
+];
+
+export { work };
