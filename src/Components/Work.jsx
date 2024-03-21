6731c7c2e547e31395339e5065c3806d5fcdc655
Shefa <shefaoudeen123@gmail.com>
2024-03-21 20:14:49 +0530

    added

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Arrow from "./Arrow";
+import { rotateIn } from "react-animations";
 
 const Work = () => {
   const [toggle, setToggle] = useState(false);
@@ -8,11 +9,15 @@ const Work = () => {
     setToggle(toggle === true ? false : true);
   };
 
+  const rotateDiv = styled.div`
+    animation: 1s ${bounceAnimation};
+  `;
+
   return (
     <div className="flex justify-center items-center w-screen ">
       <div className="flex flex-col justify-center items-center w-[100%]">
-        <div className="flex flex-col justify-center w-[80%] h-[150px] border-b-[10px] border-blue-800">
-          <h1 className="text-start text-blue-800 text-8xl font-bold">
+        <div className="flex flex-col justify-center w-[80%] h-[150px] border-b-[10px] border-[#005cef]">
+          <h1 className="text-start text-[#005cef] text-8xl font-bold">
             Our Work.
           </h1>
         </div>
@@ -20,24 +25,24 @@ const Work = () => {
           onClick={() => handleClick()}
           className={`flex flex-col justify-center w-[80%] h-[${
             toggle === true ? 1000 : 200
-          }px] border-b-[10px] border-blue-800 cursor-pointer`}
+          }px] border-b-[10px] border-[#005cef] cursor-pointer`}
         >
           <div className="flex flex-col justify-center relative h-[200px]">
-            <h1 className="text-rose-500 text-2xl font-semibold">
+            <h1 className="text-[#ff1f25] text-2xl font-semibold">
               Brand Idents
             </h1>
-            <h1 className="text-rose-500 text-7xl font-bold">MakeReign.</h1>
+            <h1 className="text-[#ff1f25] text-7xl font-bold">MakeReign.</h1>
             <div
               className={`absoulte w-[50px] h-[100px] absolute right-2 rotate-${
-                toggle === true ? 180 : 180
+                toggle === true ? 180 : null
               }`}
             >
-              <Arrow />
+              <Arrow className={`animate-${toggle === true ? "spin" : null}`} />
             </div>
           </div>
           {toggle === true ? (
-            <div className="h-[600px] flex bg-rose-600">
-              <div className="w-1/2 h-full bg-blue-800"></div>
+            <div className="h-[600px] flex bg-[#ff1f25]">
+              <div className="w-1/2 h-full bg-[#005cef]"></div>
               <div className="flex  gap-10 justify-center flex-col items-center w-1/2 h-full bg-rose-600">
                 <div className="text-white text-5xl font-bold font-sans w-[80%]">
                   <h1>MakeReign's new</h1>
