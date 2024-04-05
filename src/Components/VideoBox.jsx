4207d85df5f4fbcb5c1ec85d709ac78e22712b2c
Shefa <shefaoudeen123@gmail.com>
2024-04-05 17:47:59 +0530

    favicon changed

--- src/Components/VideoBox.jsx
+++ src/Components/VideoBox.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const VideoBox = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center border-2 border-black max-sm:text-6xl">
+    <div className="w-full h-screen flex justify-center text-center items-center border-2 border-black max-sm:text-6xl">
       <h1 className="text-8xl font-bold">VIDEO BOX</h1>
     </div>
   );
