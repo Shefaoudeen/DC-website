21b38841cea14ab69c9a44254f9a976e153f243d
Shefa <shefaoudeen123@gmail.com>
2024-04-07 20:06:57 +0530

    Translation added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,13 @@ const Home = () => {
   return (
     <div className="w-full h-screen flex justify-center items-start border-2 border-black max-sm:text-6xl">
       <section className="hero">
-        <img src="src\assets\home_square_pole.svg" alt="" className="square" />
+        <div id="anim-mask" className="">
+          <img
+            src="src\assets\home_square_pole.svg"
+            alt=""
+            className="square"
+          />
+        </div>
         <div className="wrapper">
           <h1 className="hero-title">
             Hey,&nbsp;
