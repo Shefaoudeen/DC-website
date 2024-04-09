a521c11895ec7bb6914ee0274dd0e3d4437a4a66
Kuhan S <thisiskuhan@gmail.com>
2024-04-09 07:25:24 +0530

    About Section Updated.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,8 +2,24 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="bg-[#005cef] w-full h-screen flex justify-center text-center items-center max-sm:text-6xl">
-      <h1 className="text-8xl font-bold">About</h1>
+    <div className="bg-[#005cef] w-full h-screen flex justify-start items-start max-sm:text-6xl">
+      <div className="flex flex-col justify-center w-[100%] max-sm:w-[90%] home-custom-2 pt-20">
+        <h1 className="text-[#01255e] text-8xl font-bold">Who we are?</h1>
+        <h1 className="text-[white] text-6xl font-bold pt-10">
+          Lorem ipsum dolor sit amet,&nbsp;
+          <br />
+          consectetur adipiscing elit. &nbsp;
+          <br />
+          Sed ultrices et leo sit amet &nbsp;
+          <br />
+          blandit. Nullam dictum feugiat &nbsp;
+          <br />
+          tortor non convallis. Donec &nbsp;
+          <br />
+          sollicitudin commodo mauris, &nbsp;
+          <br />a porta orci gravida ut.
+        </h1>
+      </div>
     </div>
   );
 };
