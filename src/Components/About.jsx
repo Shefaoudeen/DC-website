a7b322c0d68a082f641c127675d5844da1742e6f
Kuhan S <thisiskuhan@gmail.com>
2024-04-09 07:40:34 +0530

    About Section Updated.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,22 +3,29 @@ import React from "react";
 const About = () => {
   return (
     <div className="bg-[#005cef] w-full h-screen flex justify-start items-start max-sm:text-6xl">
-      <div className="flex flex-col justify-center w-[100%] max-sm:w-[90%] home-custom-2 pt-20">
+      <div className="flex flex-col justify-center w-[100%] max-sm:w-[90%] z-50 home-custom-2 pt-20">
         <h1 className="text-[#01255e] text-8xl font-bold">Who we are?</h1>
         <h1 className="text-[white] text-6xl font-bold pt-10">
-          Lorem ipsum dolor sit amet,&nbsp;
-          <br />
-          consectetur adipiscing elit. &nbsp;
-          <br />
-          Sed ultrices et leo sit amet &nbsp;
-          <br />
-          blandit. Nullam dictum feugiat &nbsp;
-          <br />
-          tortor non convallis. Donec &nbsp;
-          <br />
-          sollicitudin commodo mauris, &nbsp;
-          <br />a porta orci gravida ut.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices
+          et leo sit amet blandit. Nullam dictum feugiat tortor non convallis.
+          Donec sollicitudin commodo mauris, a porta orci gravida ut.
         </h1>
+        <h4 className="text-[white] pt-10 ">
+          Nullam odio nunc, placerat sit amet odio nec, gravida lobortis eros.
+          In vel volutpat dolor, at accumsan velit. Fusce egestas molestie enim,
+          vel elementum tellus euismod eget. Sed cursus augue lobortis augue
+          sollicitudin congue. Vivamus a justo est. Sed sapien ligula, porttitor
+          at tellus rutrum, lobortis porta dui. Nunc in augue erat. Donec quis
+          arcu at neque efficitur elementum. Vestibulum commodo iaculis sapien,
+          vel rutrum lectus faucibus a. Morbi faucibus dictum convallis. Cras mi
+          ipsum, facilisis eget nulla vel, commodo suscipit dolor. Pellentesque
+          nec magna facilisis ipsum consequat efficitur ac et tellus.
+          Pellentesque aliquet felis ac justo cursus, nec dignissim turpis
+          pulvinar. Suspendisse porttitor eros vitae augue venenatis, id
+          pharetra sapien congue. Sed vulputate eros leo, vulputate scelerisque
+          purus venenatis nec. Quisque vel ex tincidunt, vulputate sapien eu,
+          lobortis nunc.
+        </h4>
       </div>
     </div>
   );
