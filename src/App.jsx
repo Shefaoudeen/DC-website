2a48c6ee919dc1daa511e246fdf8b0e2f4821fd7
Shefa <shefaoudeen123@gmail.com>
2024-04-17 23:47:19 +0530

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,35 @@ import Layout from "./Pages/Layout";
 import ScrollToTop from "./Components/ScrollToTop";
 import Homepage from "./Pages/Homepage";
 import Units from "./Components/Units";
+import { useState } from "react";
+import { Logo } from "./assets";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 2500);
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Homepage />} />
-          <Route path="/units" element={<Units />} />
-          <Route path="/prime" element={<Prime />} />
-          <Route path="/video" element={<Video />} />
-          <Route path="/development" element={<Development />} />
-        </Route>
-      </Routes>
+      {loading ? (
+        <div className="flex flex-col justify-center items-center h-screen w-full bg-white">
+          <img src={Logo} className="w-[500px] h-[500px]" />
+          <h1 className="font-bold text-5xl pt-5 text-[#298892]">
+            DESIGN CLUB
+          </h1>
+        </div>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Homepage />} />
+            <Route path="/units" element={<Units />} />
+            <Route path="/prime" element={<Prime />} />
+            <Route path="/video" element={<Video />} />
+            <Route path="/development" element={<Development />} />
+          </Route>
+        </Routes>
+      )}
     </BrowserRouter>
   );
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -5,5 +5,6 @@ import HomeImage from "./home_square_pole.svg";
 import DownArrow from "./arrow-blue-long.svg";
 import UpArrow from "./arrow-white-long.svg";
 import VideoForBox from "./video-box.mp4";
+import Logo from "./logo.png";
 
-export { luffy, zoro, sanji, HomeImage, DownArrow, UpArrow, VideoForBox };
+export { luffy, zoro, sanji, HomeImage, DownArrow, UpArrow, VideoForBox, Logo };
