b034db9b24b6d33f28f044212138df9db5ace3f2
thisiskuhan <thisiskuhan@gmail.com>
2024-04-27 17:21:00 +0530

    work section updated

--- src/Data/works.js
+++ src/Data/works.js
@@ -3,55 +3,55 @@ const works = [
     title: "PTU 2024 calendar. ",
     link: "https://drive.google.com/drive/folders/1RtdnlpIox20ZVnC3kX2WPz4sQOWbAjYt?usp=drive_link",
     description:
-      "The official 2024 calendar of PTU showcases captivating images captured during various university events and at notable locations on campus. Designed by Devanathan from Prime Unit of DC, who also serves as the Vice President of DC, the calendar features stunning visuals that celebrate the spirit and essence of PTU. Each month highlights memorable moments and picturesque landscapes, offering a glimpse into the vibrant life and culture of the university community.",
+      "Under the guidance of the club's former secretary, Gokulnath, the official PTU 2024 calendar showcases captivating images captured during various university events and at notable locations on campus. Designed by Devanathan from the Prime Unit, who also serves as the club's Vice President, the calendar features stunning visuals that celebrate the spirit and essence of PTU. Each month highlights memorable moments and picturesque landscapes, offering a glimpse into the vibrant life and culture of the university community.",
   },
   {
     title: "DC101: Adobe Photoshop Workshop. ",
     link: "https://drive.google.com/drive/folders/19qceIRgbImhkRSik_ogZUW9WZ2jjVZSx?usp=sharing",
     description:
-      "In December 2023, DC hosted a hands-on workshop on the Basics of Adobe Photoshop, organized by then-Secretary Gokulnath and Chief Yokeswary Anebarassane under the guidance of Dr. K. Sathiyamurthy. The workshop, featuring keynote speakers Kuhan, the current President of DC, and Devanathan, the current Vice President of DC, provided practical training to over 40 students. Participants learned the fundamentals of Photoshop through interactive sessions and hands-on projects. Access the documentation and workshop assets through the link provided below.",
+      "In December 2023, DC hosted a hands-on workshop on the Basics of Adobe Photoshop, organized by then-Secretary Gokulnath and Chief Yokeswary Anebarassane under the guidance of Dr. K. Sathiyamurthy. The workshop, featuring keynote speakers Kuhan, the current President, and Devanathan, the current Vice President, provided practical training to over 40 students. Participants learned the fundamentals of Photoshop through interactive sessions and hands-on projects. Access the documentation and workshop assets through the link provided below.",
   },
   {
     title: "Reflections: Literary Club Newsletter Edition-1. ",
     link: "https://drive.google.com/drive/folders/1g5vJIelKZpk5oTCxAMXXPDp3s0lGtER_?usp=sharing",
     description:
-      "Reflections Edition 1: A Literary Club Newsletter, designed by Kuhan S, President of DC, and guided by Gokulnath, the then-Secretary, showcases creative content contributed by members of the Literary Club. This edition explores themes on space design, featuring captivating articles and artwork, accompanied by seven different color palettes to enhance visual appeal. Dive into the world of creativity and imagination with Reflections Edition 1.",
+      "Reflections Edition 1: A Literary Club Newsletter, designed by Kuhan S, President, and guided by Gokulnath, the then-Secretary, showcases creative content contributed by members of the Literary Club. This edition explores themes on space design, featuring captivating articles and artwork, accompanied by seven different color palettes to enhance visual appeal. Dive into the world of creativity and imagination with Reflections Edition 1.",
   },
   {
     title: "Fine Arts Club Magazine. ",
     link: "https://drive.google.com/drive/folders/1SPTT9jDbnHKn6eDcZ9ptv9boqM7Qwi18?usp=sharing",
     description:
-      "The Fine Arts Club Magazine, meticulously crafted by a collaborative team including Devanathan, Vice President of DC, Shrikanth from Prime Unit of DC, and Kuhan, President of DC, serves as a vibrant showcase of creative talent. This visually stunning publication celebrates the artistic endeavors of club members, featuring a diverse array of artwork, poetry, and creative writing. With its engaging design and captivating content, the magazine inspires and showcases the creative spirit within the university community.",
+      "The Fine Arts Club Magazine, meticulously crafted by a collaborative team including Devanathan, Vice President, Shrikanth from Prime Unit, and Kuhan, President, serves as a vibrant showcase of creative talent. This visually stunning publication celebrates the artistic endeavors of club members, featuring a diverse array of artwork, poetry, and creative writing. With its engaging design and captivating content, the magazine inspires and showcases the creative spirit within the university community.",
   },
   {
     title: "Rejouir-Pecofes. ",
     link: "https://www.instagram.com/rejouir_ptu/",
     description:
-      "Rejouir, formerly known as Pecofes, is the annual university festival that celebrates creativity, talent, and community spirit. For the past three years, every aspect of the festival, including posters, invitations, banners, mementos, and more, has been meticulously designed by Devanathan, Vice President of DC, and Shrikaanth from Prime Unit of DC. Their creative vision and dedication have played a significant role in making Rejouir a memorable and successful event.",
+      "Rejouir, formerly known as Pecofes, is the annual university festival that celebrates creativity, talent, and community spirit. For the past three years, every aspect of the festival, including posters, invitations, banners, mementos, and more, has been meticulously designed by Devanathan, Vice President, and Shrikaanth from Prime Unit. Their creative vision and dedication have played a significant role in making Rejouir a memorable and successful event.",
   },
   {
     title: "Neura 2023. ",
     link: "https://www.instagram.com/neura2k23/",
     description:
-      "Neura, a national-level symposium held in March 2023, was organized by the Department of Information Technology at PTU. The event was meticulously designed by a collaborative team including Kuhan, President of DC, Yoge from Prime Unit of DC, and Balaji, Navin, Santhosh, and Cyrill from the IT department. This dedicated team was responsible for designing everything from posters, invitations, banners to mementos, ensuring a visually appealing and well-coordinated event.",
+      "Neura, a national-level symposium held in March 2023, was organized by the Department of Information Technology at PTU. The event was meticulously designed by a collaborative team including Kuhan, President, Yoge from Prime Unit, and Balaji, Navin, Santhosh, and Cyrill from the IT department. This dedicated team was responsible for designing everything from posters, invitations, banners to mementos, ensuring a visually appealing and well-coordinated event.",
   },
   {
     title: "PTU Intro. ",
     link: "",
     description:
-      "Introducing a brand new university intro currently under production, directed by Gokulnath, Secretary of DC, and featuring performances by Manoj Kumarar with Ashwath, and Abhinavkumar from DC. The video editing is handled by the talented team from the DC Video Unit, including Abhinavkumar, Ashwath, and Kaushik, under the guidance of Gokulnath. Stay tuned for its release!",
+      "Introducing a brand new university intro currently under production, directed by Gokulnath, Secretary, and featuring performances by Manoj Kumar with Ashwath, and Abhinavkumar from DC. The video editing is handled by the talented team from the DC Video Unit, including Abhinavkumar, Ashwath, and Kaushik, under the guidance of Gokulnath. Stay tuned for its release!",
   },
   {
     title: "DC Pamphlet. ",
     link: "https://drive.google.com/drive/folders/1P_eMgcaMJnElrqK-elIolXToXa16du3_?usp=sharing",
     description:
-      "The DC (Design Club) pamphlet, introducing various units of DC, has been meticulously crafted by Kuhan, President of DC, in honor of World Design Day on April 27. The pamphlet features a minimalist design, centered around the concept of a lone wolf, symbolizing its independent and innovative spirit. Using a monochrome palette with various shades, the design emphasizes simplicity and elegance.",
+      "The DC (Design Club) pamphlet, introducing various units of DC, has been meticulously crafted by Kuhan, President, in honor of World Design Day on April 27. The pamphlet features a minimalist design, centered around the concept of a lone wolf, symbolizing its independent and innovative spirit. Using a monochrome palette with various shades, the design emphasizes simplicity and elegance.",
   },
   {
     title: "PTU Website. ",
     link: "https://ptuniv.edu.in/",
     description:
-      "9Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "The PTU website's initial version was developed by Rupam Chirom, the club founder, and his team. Following the transition of the college into a university, Aadhitya, the former secretary, and his team took on the task of redesigning and developing the new frontend. Subsequently, Gokulnath, who served as the secretary, took over the responsibility of maintaining the website.",
   },
   {
     title: "Placement Guide. ",
@@ -69,13 +69,13 @@ const works = [
     title: "DC Portfolio. ",
     link: "",
     description:
-      "9Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "The DC Portfolio Website stands as the first project of the Development Unit in the DC'24 slate. This innovative website, developed by Shefaoudeen from the Development Unit, in collaboration with Kuhan, President, showcases the club's creative prowess. Employing a unique and unconventional style, the website features vibrant basic colors that accentuate design elements, reflecting the club's commitment to creativity and innovation.",
   },
   {
     title: "Explore More.",
     link: "https://ptuniv.edu.in/4c/design-club",
     description:
-      "9Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "The Design Club, established in 2019, has grown over the years with the contributions of various club members. Together, they have made significant contributions to projects such as the PTU website, Rejouir, calendars, and more. Explore all our works on the university website and witness the creative journey of the Design Club.",
   },
 ];
 
